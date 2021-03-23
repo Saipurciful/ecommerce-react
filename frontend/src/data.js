8f4419552a922377ac1604284d2f94ec36b0db59
Sai Purciful <saipurciful@gmail.com>
2021-03-22 22:45:02 -0700

    added frontend folder

---
+++ frontend/src/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Embroidery dog leash',
+            category: 'dog collar',
+            image: '/images/collar1.jpg',
+            price: 50.00,
+            brand: 'collar',
+            rating: 4,
+            numReviews: 4,
+            description: 'Embroidered dog collar, personalized custom, name and/or phone number, 25 nylon colors, lots of thread and font choices, nylon adjustable',
+
+        },
+        {
+            _id: '2',
+            name: 'Embroidery dog collar 2',
+            category: 'dog collar',
+            image: '/images/collar2.jpg',
+            price: 50.00,
+            brand: 'collar',
+            rating: 4,
+            numReviews: 4,
+            description: 'Embroidered dog collar, personalized custom, name and/or phone number, 25 nylon colors, lots of thread and font choices, nylon adjustable',
+
+        },
+        {
+            _id: '3',
+            name: 'Embroidery dog collar 3',
+            category: 'dog Leash',
+            image: '/images/collar1.jpg',
+            price: 50.00,
+            brand: 'collar',
+            rating: 4,
+            numReviews: 4,
+            description: 'Embroidered dog collar, personalized custom, name and/or phone number, 25 nylon colors, lots of thread and font choices, nylon adjustable',
+
+        },
+        {
+            _id: '4',
+            name: 'Embroidery dog collar 4',
+            category: 'Dog Leash',
+            image: '/images/collar2.jpg',
+            price: 50.00,
+            brand: 'collar',
+            rating: 4,
+            numReviews: 4,
+            description: 'Embroidered dog collar, personalized custom, name and/or phone number, 25 nylon colors, lots of thread and font choices, nylon adjustable',
+
+        },
+        {
+            _id: '5',
+            name: 'Embroidery dog collar 5',
+            category: 'dog collar',
+            image: '/images/collar1.jpg',
+            price: 50.00,
+            brand: 'collar',
+            rating: 4,
+            numReviews: 4,
+            description: 'Embroidered dog collar, personalized custom, name and/or phone number, 25 nylon colors, lots of thread and font choices, nylon adjustable',
+
+        },
+        {
+            _id: '6',
+            name: 'Embroidery dog collar 6',
+            category: 'dog collar',
+            image: '/images/collar2.jpg',
+            price: 50.00,
+            brand: 'collar',
+            rating: 4,
+            numReviews: 4,
+            description: 'Embroidered dog collar, personalized custom, name and/or phone number, 25 nylon colors, lots of thread and font choices, nylon adjustable',
+
+        },
+
+    ]
+}
+
+export default data
