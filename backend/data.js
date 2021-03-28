025cee1d7df98664dace93d9483deefe1ec6c147
Sai Purciful <saipurciful@gmail.com>
2021-03-28 02:08:10 -0700

    created sample users and connected to MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+    users: [
+        {
+            name: 'Sai',
+            email: 'sai@gmail.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'John',
+            email: 'user@gmail.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
             _id: '1',
